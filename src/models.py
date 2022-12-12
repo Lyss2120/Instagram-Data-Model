ab0e6b273b44232d4da4b85dfe3d6c7908104105
Lyss2120 <Lys.ivonne@gmail.com>
2022-12-12 18:48:21 +0000

    listo

--- src/models.py
+++ src/models.py
@@ -8,23 +8,67 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__= 'user'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    Username = Column(String(250))
+    Name = Column(String(250))
+    Lastname = Column(String(250))
+    Password = Column(String(250))
+
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    Video_Photo = Column(String(250)) 
+    Description = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))        
+    user = relationship(User)
+
+class Follower(Base):
+    __tablename__= 'follower'
+    id = Column(Integer, primary_key=True)
+    Username = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))        
+    user = relationship(User)
+
+class Suggestions(Base):
+    __tablename__='suggestions'
+    id = Column(Integer, primary_key=True) 
+    Username = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))        
+    user = relationship(User)
+
+
+class Likes(Base):
+    __tablename__='likes'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))        
+    user = relationship(User)
+    media_id = Column(Integer, ForeignKey('media.id'))
+    media = relationship('Media')
+
+
+class Saved(Base):
+    __tablename__= 'saved'
+    id = Column(Integer, primary_key=True)
+    Video_Photo = Column(String(250)) 
+    user_id = Column(Integer, ForeignKey('user.id'))        
+    user = relationship(User)
+    media_id = Column(Integer, ForeignKey('media.id'))
+    media = relationship(Media)
+
+
+class Comments(Base):
+    __tablename__='comments'
+    id = Column(Integer, primary_key=True)  
+    media_id = Column(Integer, ForeignKey('media.id'))
+    media = relationship(Media)
+    user_id = Column(Integer, ForeignKey('user.id'))        
+    user = relationship(User)
+
+
 
     def to_dict(self):
         return {}
@@ -36,3 +80,5 @@ try:
 except Exception as e:
     print("There was a problem genering the diagram")
     raise e
+
+
